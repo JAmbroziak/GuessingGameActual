faad37025af0a501003a63a23b5c74e984e17193
fpsft <jambroziak9340@bths.edu>
2018-10-13 13:30:54 -0400

    Objective2 Final

--- src/Objective2.java
+++ src/Objective2.java
@@ -6,24 +6,33 @@ public class Objective2 {
         Scanner input = new Scanner(System.in);
         double guess = 50;
         double change = 25;
+        int guessCount = 1;
         System.out.println("Is your number "+guess+"?");
         String answer = input.nextLine();
-        while(!answer.equals("correct"))
-        {
-            if(answer.equals("higher"))
-            {
-                guess += (int)(change);
-                change += (1.5*change);
+        while(!answer.equals("correct")) {
+            if (answer.equals("higher")) {
+                guess += (int) (change);
                 answer = "";
             }
-            if(answer.equals("lower"))
-            {
-                guess -= (int)(change);
-                change -= (change*.5);
+            if (answer.equals("lower")) {
+                guess -= (int) (change);
                 answer = "";
             }
+            if (change < 2)
+            {
+                change = 1;
+            } else
+            {
+                change = (change * .5);
+            }
+            guessCount++;
             System.out.println("Is your number " + guess + "?");
             answer = input.nextLine();
         }
+        if(guessCount == 1)
+        {
+            System.out.println("Wow, try harder. I got that in 1 guess.");
+        } else
+        System.out.println("Nice job, you made me take "+guessCount+" guesses.");
     }
 }
